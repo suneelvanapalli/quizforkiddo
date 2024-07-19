e4d3fbb111dcb041f7a47469c03789efc25d6091
Suneel Kumar Vanapalli <suneel.vanapalli@gmail.com>
2024-07-19 17:39:50 +1000

    js custom action to deploy to gh pages

---
+++ .github/actions/custom-js-actions/index.js
@@ -0,0 +1,11 @@
+import core from '@actions/core';
+import exec from '@actions/exec';
+import github from '@actions/github';
+
+const run = () => {
+  const distFolder = core.getInput('dist-folder', { required: true });
+  exec.exec(`gh-pages -d ${distFolder}`);
+  //core.notice('Hello! from custom github action');
+};
+
+run();
